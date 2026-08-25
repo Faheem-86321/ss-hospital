ec20160ea0d926cca03101b202bfa130f2062424
Faheem-86321 <devfaheem86@gmail.com>
2026-08-25 13:19:57 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,55 +2,25 @@
 session_start();
 ob_start(); 
 
-if(!isset($_GET['page'])){
-    $_GET['page'] = "Login";
-}
-
-// Try both lowercase and uppercase versions of the filename
-$page_lower = "web_temp/". strtolower($_GET['page']) . ".html.php";
-$page_upper = "web_temp/". ucfirst(strtolower($_GET['page'])) . ".html.php";
-
-$page_file = null;
-if(file_exists($page_lower)) {
-    $page_file = $page_lower;
-} elseif(file_exists($page_upper)) {
-    $page_file = $page_upper;
-}
-
-if($page_file) 
-{
-    /* Common Header */
-    $head_title = $_GET['page'];
-    
-    // Only include if they exist
-    if(file_exists("models/logincookie.php")) {
-        include_once("models/logincookie.php");
+// DEBUG: List all files in web_temp folder
+echo "<h2>Debug: Files in web_temp/</h2>";
+if(is_dir("web_temp")) {
+    $files = scandir("web_temp");
+    echo "<ul>";
+    foreach($files as $file) {
+        if($file != "." && $file != "..") {
+            echo "<li>" . $file . "</li>";
+        }
     }
-    if(file_exists("env/main_config.php")) {
-        include_once("env/main_config.php");
-    }
-    if(file_exists("common/header.php")) {
-        include_once("common/header.php");
-    }
-    
-    include_once($page_file);
-    
-    if(file_exists("common/footer.php")) {
-        include_once("common/footer.php");
-    }
+    echo "</ul>";
 } else {
-    // 404 Not Found
-    ?>
-    <!DOCTYPE html>
-    <html>
-    <head><title>404 - Page Not Found</title></head>
-    <body style="text-align:center; padding:50px; font-family:Arial;">
-        <h1>404 - Page Not Found</h1>
-        <p>The page "<?php echo $_GET['page']; ?>" does not exist.</p>
-        <p><a href="?page=login">Go to Login</a></p>
-    </body>
-    </html>
-    <?php
-    die();
+    echo "<p>web_temp folder does not exist!</p>";
 }
-?>
+echo "<hr>";
+// END DEBUG
+
+if(!isset($_GET['page'])){
+    $_GET['page'] = "login";
+}
+
+// ... rest of your code ...
