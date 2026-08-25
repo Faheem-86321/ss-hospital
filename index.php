e0812a0796658180a8d113e099c324ace830b2e5
Faheem-86321 <devfaheem86@gmail.com>
2026-08-25 13:17:08 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,56 @@
 <?php 
-  session_start();
-  ob_start(); 
+session_start();
+ob_start(); 
+
 if(!isset($_GET['page'])){
-  $_GET['page']= "login";
+    $_GET['page'] = "login";
 }
-if(file_exists("web_temp/". $_GET['page'].".html.php")) 
+
+// Try both lowercase and uppercase versions of the filename
+$page_lower = "web_temp/". strtolower($_GET['page']) . ".html.php";
+$page_upper = "web_temp/". ucfirst(strtolower($_GET['page'])) . ".html.php";
+
+$page_file = null;
+if(file_exists($page_lower)) {
+    $page_file = $page_lower;
+} elseif(file_exists($page_upper)) {
+    $page_file = $page_upper;
+}
+
+if($page_file) 
 {
-  /* Common Header */
-  $head_title = $_GET['page'];
-  include_once("models/logincookie.php");
-  include_once("env/main_config.php");
-  include_once("common/header.php");
-  
-  include_once("web_temp/". $_GET['page'].".html.php");
-  include_once("common/footer.php");
-}else{
-  ?>
-  <img src="images/html-404-error.jpg" width="100%" height="100%">
-  <?php
-  die();
+    /* Common Header */
+    $head_title = $_GET['page'];
+    
+    // Only include if they exist
+    if(file_exists("models/logincookie.php")) {
+        include_once("models/logincookie.php");
+    }
+    if(file_exists("env/main_config.php")) {
+        include_once("env/main_config.php");
+    }
+    if(file_exists("common/header.php")) {
+        include_once("common/header.php");
+    }
+    
+    include_once($page_file);
+    
+    if(file_exists("common/footer.php")) {
+        include_once("common/footer.php");
+    }
+} else {
+    // 404 Not Found
+    ?>
+    <!DOCTYPE html>
+    <html>
+    <head><title>404 - Page Not Found</title></head>
+    <body style="text-align:center; padding:50px; font-family:Arial;">
+        <h1>404 - Page Not Found</h1>
+        <p>The page "<?php echo $_GET['page']; ?>" does not exist.</p>
+        <p><a href="?page=login">Go to Login</a></p>
+    </body>
+    </html>
+    <?php
+    die();
 }
 ?>
-
